ae62b26539aa5ff0395bea1537e35247ef8b80c6
Shuhei Hiya <syuhei176@gmail.com>
2017-01-29 23:41:49 +0900

    Added express

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict'
 
 const jmx = require("jmx");
+const express = require('express');
+const app = express();
+
+let storageServiceData = {};
+
+
+app.get('/db/:type', function(req, res) {
+	let type = req.params.type;
+	if(type == 'ss') {
+		res.json({err:null,content:storageServiceData});
+	}else{
+		res.json({err:null});
+	}
+});
+
+app.get('/db/:type/:name', function(req, res) {
+	let type = req.params.type;
+	let name = req.params.name;
+	if(type == 'ss') {
+		res.json({err:null,content:storageServiceData[name]});
+	}else{
+		res.json({err:null});
+	}
+});
+
+app.listen(3000);
 
 let client = jmx.createClient({
 	host: "localhost",
@@ -30,10 +56,21 @@ client.on("connect", function() {
 		console.log('999thPercentile: ', data.toString()); 
 	});
 
+	startTask();
+
+});
+
+function startTask() {
+	setInterval(task, 10 * 1000);
+	task();
+}
+
+function task() {
 	const StorageServiceNames = ['LiveNodes', 'LoadMap', 'JoiningNodes', 'MovingNodes', 'LeavingNodes', 'Starting', 'Keyspaces'];
 	StorageServiceNames.forEach(function(name) {
 		client.getAttribute("org.apache.cassandra.db:type=StorageService", name, function(data) {
+			storageServiceData[name] = data.toString();
 			console.log(`${name}: `, data.toString()); 
 		});
 	});
-});
+}
