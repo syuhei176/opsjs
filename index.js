f1b3fcdd1aeb5eea58c76897a2ac6e0e9f09c746
Shuhei Hiya <syuhei176@gmail.com>
2017-01-29 22:43:35 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const jmx = require("jmx");
+
+let client = jmx.createClient({
+	host: "localhost",
+	port: 7199
+});
+ 
+client.connect();
+
+client.on("connect", function() {
+	client.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", function(data) {
+		var used = data.getSync('used');
+		console.log("HeapMemoryUsage used: " + used.longValue);
+	});
+
+	console.log('Read,Latency');
+
+	client.getAttribute('org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Latency', "95thPercentile", function(data) {
+		console.log('95thPercentile: ', data.toString()); 
+	});
+	client.getAttribute('org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Latency', "98thPercentile", function(data) {
+		console.log('98thPercentile: ', data.toString()); 
+	});
+	client.getAttribute('org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Latency', "99thPercentile", function(data) {
+		console.log('99thPercentile: ', data.toString()); 
+	});
+	client.getAttribute('org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Latency', "999thPercentile", function(data) {
+		console.log('999thPercentile: ', data.toString()); 
+	});
+
+	client.getAttribute("org.apache.cassandra.db:type=StorageService", "LiveNodes", function(data) {
+		console.log('LiveNodes: ', data.toString()); 
+	});
+});
